0919ff9a105d29df76173588a0d9ae39c1962744
vagrant <vagrant@precise64.(none)>
2014-01-16 11:44:00 +0000

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Proofreading123::Application.config.secret_key_base = '17ee4897cf5cdf6e069336199d902fb9ee8dc76fa7dda94aebb750e57ade78b39d4a8c2eb12368d36f49f4e8a0eb580b3acd585f7c0f6d30494f4832cc0df4d3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Proofreading123::Application.config.session_store :cookie_store, key: '_proofreading123_session'
